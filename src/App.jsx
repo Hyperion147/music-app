74c3478634695550424b93a5ca7f26ae5719fc9c
tarun <tarunjangra1903@gmail.com>
2025-12-16 22:19:46 +0530

    shadcn and firebase installation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {Button } from "./components/ui/button.jsx"
 import './App.css'
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
   return (
     <>
       <h1 className='text-center'>Music App</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </>
   )
 }
